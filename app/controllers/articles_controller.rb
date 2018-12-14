e88a476d7844eb8ef3b6f54ab8d99958b5ee6cbb
sachinvj <sachin73@gmail.com>
2018-12-14 10:20:49 +0000

    EDIT show Actions

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,11 @@
 class ArticlesController < ApplicationController
 
+
+def index 
+ @articles = Article.all
+end
+
+
 def new 
 
 @article = Article.new
@@ -9,12 +15,38 @@ end
 def create 
 
 @article = Article.new(article_params)
+
 if @article.save
  flash[:notice] = "Article was successfuly created"
  redirect_to article_path(@article)
 else 
  render 'new'
 end
+
+end
+
+def edit
+ @article = Article.find(params[:id])
+end
+
+def update
+ @article = Article.find(params[:id])
+ 
+ if @article.update(article_params)
+  flash[:notice] = "Article was successfully updated"
+   redirect_to article_path(@article)
+ else
+  render 'edit'
+ end
+
+
+end 
+
+
+def show
+
+@article = Article.find(params[:id])
+
 end
 
 private 
@@ -22,12 +54,6 @@ private
 def article_params
 
 params.require(:article).permit(:title, :description)
-
-end
-
-def show
-
-@article = Article.find(params[:id])
 
 end
 
